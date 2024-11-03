99bb9be87654f4564b58c6092551ee2973dbe6ec
samkiyya <samuelkiyya2012@gmail.com>
2024-11-03 05:57:23 -0800

    react card using tachyons css toolkit

--- vite_robo/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-function App(props) {
-  return (
-    <>
-      <h1>{props.greeting}</h1>
-    </>
-  );
-}
-export default App;
---
+++ vite_robo/src/Card.jsx
@@ -0,0 +1,12 @@
+const Card = () => {
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src="https://robohash.org/sam" alt="photo of sam" />
+      <div>
+        <h2>Samuel Aberra</h2>
+        <p>samuelabera523@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+export default Card;
