19eb5b62ef8081d938c70ac490a810b99aa9b1cd
samkiyya <samuelkiyya2012@gmail.com>
2024-11-03 05:20:01 -0800

    react class vs hooks in functional components

---
+++ vite_robo/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+function App(props) {
+  return (
+    <>
+      <h1>{props.greeting}</h1>
+    </>
+  );
+}
+export default App;
---
+++ vite_robo/src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App greeting={"hello sam"} />
+  </StrictMode>
+);
+
+// // importing React and ReactDom from the "react" and "react-dom" packages, respectively.
+// import React from "react";
+// import ReactDom from "react-dom";
+// import "./index.css";
+// import App from "./App.jsx";
+// const root = document.getElementById("root");
+// ReactDom.createRoot(root).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+/*// importing React and ReactDom from the "react" and "react-dom" packages, respectively.
+import {StrictMode} from "react";
+import {createRoot} from "react-dom";
+import "./index.css";
+import App from "./App.jsx";
+const root = document.getElementById("root");
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)*/
